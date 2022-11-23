d4e1febdc3b3c65a27ec6238f59fd0e9286d4bd8
Pau Fernandez Bardulet <paufernandezbardulet@outlook.com>
2022-11-23 21:59:02 +0100

    Test resource versioning

--- src/java/service/RESTappV1.java
+++ src/java/service/RESTappV1.java
@@ -1,7 +1,22 @@
 package service;
 
+import authn.RESTRequestFilter;
 import jakarta.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+import testresources.HelloWorldResourceV1;
 
 @jakarta.ws.rs.ApplicationPath("/rest/api/v1")
 public class RESTappV1 extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<Class<?>>();
+        classes.add(AbstractFacade.class);
+        classes.add(CryptocurrencyFacadeREST.class);
+        classes.add(CustomerFacadeREST.class);
+        classes.add(PurchaseFacadeREST.class);
+        classes.add(RESTRequestFilter.class);
+        classes.add(HelloWorldResourceV1.class);
+        return classes;
+    }
 }
--- src/java/service/RESTappV2.java
+++ src/java/service/RESTappV2.java
@@ -1,9 +1,22 @@
 package service;
 
+import authn.RESTRequestFilter;
 import jakarta.ws.rs.core.Application;
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Set;
+import testresources.HelloWorldResourceV2;
 
 @jakarta.ws.rs.ApplicationPath("/rest/api/v2")
 public class RESTappV2 extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<Class<?>>();
+        classes.add(AbstractFacade.class);
+        classes.add(CryptocurrencyFacadeREST.class);
+        classes.add(CustomerFacadeREST.class);
+        classes.add(PurchaseFacadeREST.class);
+        classes.add(RESTRequestFilter.class);
+        classes.add(HelloWorldResourceV2.class);
+        return classes;
+    }
 }
